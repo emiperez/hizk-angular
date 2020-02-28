f88b45bf58a41f6c2a65380673e3a4b8ab787532
emiperez <dev@emiperez.com>
2020-02-28 19:49:22 +0000

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { TranslationsComponent } from './translations/translations.component';
+import { ExamComponent } from './exam/exam.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+	{path: "", component: TranslationsComponent },
+	{path: "exam", component: ExamComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
